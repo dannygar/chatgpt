c5c49e7f571002c60b58e3ed72bba90156a97080
Danny Garber <dannyg@outlook.com>
2023-06-27 09:59:10 -0400

    added multi-pdf support

--- app.py
+++ app.py
@@ -1,103 +1,196 @@
 import os
+import pickle
 import streamlit as st
 from dotenv import load_dotenv
-import pickle
 from streamlit_extras.add_vertical_space import add_vertical_space
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.llms import OpenAI
+from langchain.llms import AzureOpenAI, HuggingFaceHub
+from langchain.chat_models import AzureChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplate import bot_template, user_template, css
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf_doc in pdf_docs:
+        pdf_reader = PdfReader(pdf_doc)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        store_name = pdf_doc.name[:-4]
+        st.session_state.store.append(store_name)
+
+    return text
 
 
-#Sidebar content
-with st.sidebar:
-    st.title("Docs Analytics Chat Bot")
-    st.markdown('''
-    ## About
-    This app is an LLM-powered chatbot built to analyze documents and answer questions about them.
-    It is powered by:
-    - [Streamlit](https://streamlit.io)
-    - [LangChain](https://python.langchain.com/)
-    - [Azure OpenAI](https://azure.microsoft.com/en-us/products/cognitive-services/openai-service/)
+
+def get_text_chunks_recursively(raw_text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n", "\r\n", "\r", "\t", " "],
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text=raw_text)
+    return chunks
+
+
+def get_text_chunks(raw_text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text=raw_text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings(
+        deployment=os.getenv("AZURE_OPENAI_API_EMBEDDINGS_DEPLOYMENT_NAME") or '',
+        model=os.getenv("AZURE_OPENAI_API_EMBEDDINGS_MODEL_NAME") or '',
+        # openai_api_base=os.getenv("AZURE_OPENAI_API_BASE") or '',
+        # openai_api_type=os.getenv("AZURE_OPENAI_API_TYPE") or 'azure',
+        chunk_size=1,
+        client=None,
+    )        
+    VectorStore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+
+    # with open(f"{st.store_name}.pkl", "wb") as f:
+    #     pickle.dump(VectorStore, f)    
+
+    return VectorStore
     
-    ''')
-    add_vertical_space(5)
-    st.write("Made by Danny Garber, 2023")
+
+def get_vectorstore_fromHugging(text_chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    VectorStore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+
+    return VectorStore
 
 
-load_dotenv()    
-def main():
-    st.header("Chat with Docs Analytics Bot")
+def get_conversation_chain(vector_store):
+    # llm = AzureOpenAI(
+    #     deployment_name=os.getenv("AZURE_OPENAI_API_COMPLETIONS_DEPLOYMENT_NAME") or '',
+    #     client=None,
+    #     model=os.getenv("AZURE_OPENAI_API_COMPLETIONS_MODEL_NAME") or '',
+    #     temperature=0.9,
+    # )
+    llm_chat = AzureChatOpenAI(
+        deployment_name=os.getenv("AZURE_OPENAI_API_CHAT_DEPLOYMENT_NAME") or '',
+        client=None,
+        temperature=0.5,
+    )
+    # llm_hugging = HuggingFaceHub(client=None, repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
     
-    log_level = os.getenv("LOG_LEVEL", "INFO")
-    
-    # upload a PDF file
-    pdf = st.file_uploader("Upload a PDF file", type=["pdf"])
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm_chat, 
+        # llm=llm_hugging,
+        retriever=vector_store.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+     
 
-    if pdf is not None:
-        # read the PDF file
-        pdf_reader = PdfReader(pdf)
-        
-        #st.write(chunks)
-        
-        # embeddings
-        store_name = pdf.name[:-4]
+def handle_user_input(user_question):
+    response = st.session_state.conversation({"question": user_question})
+    st.session_state.history = response["chat_history"]
 
-        if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
-            if (log_level == "TRACE"):
-                st.write("Loaded existing embeddings vector from the local disk")
+    for i, message in enumerate(st.session_state.history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
         else:
-            if (log_level == "TRACE"):
-                st.write("Created new embeddings vector and saved to the local disk")
-            text = ""
-            for page in pdf_reader.pages:
-                text += page.extract_text()
-        
-            # st.write(text)
-            text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size=1000,
-                chunk_overlap=200,
-                length_function=len
-            )
-            chunks = text_splitter.split_text(text=text)
-            embeddings = OpenAIEmbeddings(
-                deployment=os.getenv("AZURE_OPENAI_API_EMBEDDINGS_DEPLOYMENT_NAME"),
-                model=os.getenv("AZURE_OPENAI_API_EMBEDDINGS_MODEL_NAME"),
-                chunk_size=1
-            )        
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            with open(f"{store_name}.pkl", "wb") as f:
-                pickle.dump(VectorStore, f)    
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+def main():
+    load_dotenv()    
+    log_level = os.getenv("LOG_LEVEL", "INFO")
+    st.set_page_config(page_title="Docs Analytics Chat Bot", page_icon=":female-doctor:")
+
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = None
+    if "store" not in st.session_state:
+        st.session_state.store = []
+
+    st.header("Chat with Medical Practice Assistant :female-doctor:")
+    user_question = st.text_input("Ask questions about the patient :male-pilot:")
+    
+    # if user_question:
+
+    #     if st.session_state.vector_store:
+    #         docs = st.session_state.vector_store.similarity_search(query=user_question, k=3)
+    #         if (log_level == "TRACE"):
+    #             # st.write(docs)
+    #             st.write("Similarity search completed")
+    
+    #         # LLM
+    #         llm = AzureOpenAI(
+    #             deployment_name=os.getenv("AZURE_OPENAI_API_COMPLETIONS_DEPLOYMENT_NAME") or '',
+    #             client=None,
+    #             model=os.getenv("AZURE_OPENAI_API_COMPLETIONS_MODEL_NAME") or '',
+    #             temperature=0.9,
+    #         )
+    #         chain = load_qa_chain(llm=llm, chain_type="stuff")
+    #         with get_openai_callback() as cb:
+    #             response = chain.run(input_documents=docs, question=user_question)
+    #             print(cb)
+    #         st.write(response)
+    
+    
+    if user_question and st.session_state.conversation:
+        handle_user_input(user_question)
+
+    #Sidebar content
+    with st.sidebar:
+        st.subheader("Documents :books:")
+        pdf_docs = st.file_uploader(
+            "Upload PDF documents here and click on 'Process'", type=["pdf"], accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing..."):
+                # get the text from the PDF
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                
+                # create vector store
+                st.session_state.vector_store = get_vectorstore(text_chunks)
+                
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(st.session_state.vector_store)
+                
             if (log_level == "TRACE"):
                 st.write("Embeddings computation completed")
 
-        # Accept user questions/queries
-        query = st.text_input("Ask questions about the document:")
-        if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
-            if (log_level == "TRACE"):
-                st.write(docs)
-                st.write("Similarity search completed")
+        add_vertical_space(5)        
+        st.markdown('''
+        ## About
+        This app is an LLM-powered chatbot built to analyze documents and answer questions about them.
+        It is powered by:
+        - [Streamlit](https://streamlit.io)
+        - [LangChain](https://python.langchain.com/)
+        - [Azure OpenAI](https://azure.microsoft.com/en-us/products/cognitive-services/openai-service/)
         
-            # LLM
-            llm = OpenAI(
-                deployment_id=os.getenv("AZURE_OPENAI_API_COMPLETIONS_DEPLOYMENT_NAME"),
-                model_name=os.getenv("AZURE_OPENAI_API_COMPLETIONS_MODEL_NAME"),
-                # deployment_id="gpt35-turbo",
-                # model_name="gpt-3.5-turbo",
-                temperature=0.9,
-            )
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                # response = chain.run(input_documents=docs, question=query)
-                response = chain.run(input_documents=docs, question=query)
-                print(cb)
-            st.write(response)
+        ''')
+        add_vertical_space(5)
+        st.write("Made by Danny Garber, 2023")
+
+     
+    
+    
             
 if __name__ == "__main__":
     main()
